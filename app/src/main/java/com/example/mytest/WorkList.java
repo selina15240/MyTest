deddec2484659d2a44454fa48f9bc605a1e37b4a
selina15240 <selina15240@naver.con>
2021-07-03 19:48:34 +0900

    Initial commit

---
+++ app/src/main/java/com/example/mytest/WorkList.java
@@ -0,0 +1,21 @@
+package com.example.mytest;
+
+public class WorkList {
+    public String wGoTime;
+    public String wOffTime;
+
+    public String getwGoTime() {
+        return wGoTime;
+    }
+
+    public String getwOffTime() {
+        return wOffTime;
+    }
+    public void setwGoTime(String wGoTime) {
+        this.wGoTime = wGoTime;
+    }
+
+    public void setwOffTime(String wOffTime) {
+        this.wOffTime = wOffTime;
+    }
+}
